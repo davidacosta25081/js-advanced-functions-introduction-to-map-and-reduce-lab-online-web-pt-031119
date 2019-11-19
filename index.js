f3fcfd7528cd7fe56c32161e6b117f0864fa2946
David Acosta <davidacosta25081@yahoo.com>
2019-11-19 00:18:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function mapToNegativize(array) {
   let newArr = []
   for (let i = 0; i < array.length; i++) {
-    newArr.push(-1 * array[i]) 
+    newArr.push(-1 * array[i])
   }
   return newArr;
 }  
