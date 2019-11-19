321edf76608f938e683b1faab7bfe8e09a23a7a5
David Acosta <davidacosta25081@yahoo.com>
2019-11-19 00:25:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,12 @@ function mapToNegativize(array) {
   }
   return newArr;
 }  
+
+
+function mapToNoChange(array) {
+  let newArr = []
+  for (let i = 0; i < array.length; i++) {
+    newArr.push(array[i])
+  }
+  return newArr;
+}  
